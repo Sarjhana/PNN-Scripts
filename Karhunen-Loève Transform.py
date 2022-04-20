ebd4a38c273c65f540be5a0fb41ec811295003b8
Sarjhana <41203054+Sarjhana@users.noreply.github.com>
2022-04-20 17:19:39 +0100

    Update Karhunen-Loève Transform.py

--- Karhunen-Loève Transform.py
+++ Karhunen-Loève Transform.py
@@ -1,38 +1,56 @@
-# Output first row should be multiplied by a -ve sign --> BUG in code
+# Output negative signs are wrong - BUG
 
+import pandas as pd
+from sklearn.decomposition import PCA
 
-import numpy as np
-from scipy.linalg import svd
-
-
-def transform(ip, n_components, data_to_project=[]):
-    ip = np.array(ip)
-    N, D = ip.shape
-    ip_mean = np.mean(ip, axis=0)
-    ip_prime = ip - ip_mean
-    ip_prime = ip_prime.T
-    C = (ip_prime) @ (ip_prime.T)
-    C = C / N
-    V, D, VT = svd(C)
-    p_input = VT @ (ip_prime)
-    print("-"*100)
-    print("Diagonal matrix [EIGEN VALUES]")
-    print(np.round(np.diag(D), 3))
-    print()
-    print("EIGEN VECTOR[TRANSPOSED]")
-    print(VT)
-    print()
-    print(
-        "PROJECTION OF INPUT [READ  COLUMNS FROM TOP, TILL N_COMPONENTS ROWS]")
-    print(np.round(p_input, 3))
-    if len(data_to_project) > 0:
-        print("PROJECTION OF TEST DATA")
-        p_given_data = VT @ data_to_project.T
-        print("READ COLUMN WISE")
-        print(p_given_data)
+def karhunen(data):
+    
+    # First we calculate the mean of each column
+    means = np.zeros(len(data.columns))# for all columns
+    for i in range(len(data.columns)):
+        means[i] = data.iloc[:, i].mean()
+    
+    mean_zero = np.zeros(data.shape)
+    # Now we substract the corresponding mean to all the values of our dataset
+    for j in range(len(data)): # for all instances
+        mean_zero[j, :] = data.iloc[j, :] - means
+        
+    # Now we have to calculate the covariance matrix
+    cov = np.zeros((len(data.columns), len(data.columns)))
+    for i in range(len(data)): # for all instances
+        cov += np.array([mean_zero[i, :]]).T*mean_zero[i, :]
+    cov = cov/len(data) # This is our final covariance matrix
+    
+    # Lastly, we obtain the eigenvalues and its corresponding eigenvectors
+    eigenVal, eigenVec = np.linalg.eig(cov)
+    
+    return means, cov, eigenVal, eigenVec
 
 # Modify Input Data
-input = [[1, 2, 1], [2, 3, 1], [3, 5, 1], [2, 2, 1]]
-data_to_project = []
-n_components = 2
-transform(input, n_components=n_components, data_to_project=data_to_project)
+
+x = pd.DataFrame([[1., 2., 1.], [2, 3., 1.], [3., 5., 1.], [2., 2., 1.]]) # input data
+
+means, cov, eigenVal, eigenVec = karhunen(x)
+
+print("Means: {}\n\nCovariance Matrix: \n{}\n\nEigenValues: {}\n\nEigenVectors: \n{}".format(means, cov, eigenVal, eigenVec))
+
+# We choose the two largest:
+val = np.array([eigenVal[1], eigenVal[0]])
+v = np.array([eigenVec[:, 1], eigenVec[:, 0]]).T
+print("\n\nEigenValues for the 2 first principal components: {}\n\nEigenVectors for the 2 first principal components: \n{}".format(val, v))
+
+# Now, for a point you use: 
+y = np.dot(v.T, (x - means).T)
+print("\n\nPoint before PCA: \n{}\n\nPoint with mean zero: \n{}\n\nProjection into the two first principal components: \n{}".format(np.array(x), np.array(x - means), y))
+
+# FASTER WAY TO DO IT WITH SCIKIT LEARN
+pca=PCA(n_components=2)
+ytest=pca.fit(x).transform(np.array(x)) # for the first point
+print("\n\nNew Points: \n{}".format(ytest.T))
+
+# Now we have to calculate the covariance matrix
+cov = np.zeros((y.shape[0],y.shape[0]))
+for i in range(y.shape[1]): # for all instances
+    cov += np.array([y[:, i]]).T*y[:, i]
+cov = cov/y.shape[1] # This is our final covariance matrix
+print("\nNew covariance matrix: \n{}".format(cov))
