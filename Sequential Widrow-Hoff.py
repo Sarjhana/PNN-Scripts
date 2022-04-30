0173a489f96288bd0596c0c829de0521cb882137
jagpdev <78238367+jagpdev@users.noreply.github.com>
2022-04-30 17:15:45 +0100

    Add files via upload

--- Sequential Widrow-Hoff.py
+++ Sequential Widrow-Hoff.py
@@ -3,63 +3,49 @@
 import numpy as np
 # ------------------------------------------------------------------------------------
 a = [1.0, 0.0, 0.0]           # Initial value of a/weights
-b = np.array([1.0, 0.5, 0.5, 0.5, 0.5, 1.0]) # Margin Vector
+b = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])  # Margin Vector
 n = 0.1                 # Learning Rate
+theta = 0.1               # Error
 iterations = 12         # Iterations
-
+AUTO_CONVERGE = False   # Ignore iterations varible, carry on until convergences
 
 # Given Dataset:
 
 # Add dataset as given in question. This is assuming that Sample Normalisation has NOT been applied:
-X = [[0,2],[1,2],[2,1],[-3,1],[-2,-1],[-3,-2]]
-Y = [1,1,1,-1,-1,-1]
+X = [[0, 2], [1, 2], [2, 1], [-3, 1], [-2, -1], [-3, -2]]
+Y = [1, 1, 1, -1, -1, -1]
 
 # ------------------------------------------------------------------------------------
 # Applying Sample Normalisation:
-Norm_Y = []
+NORM_X = []
 
 for x, y in zip(X, Y):
-    
-    # If the sample belongs to the class 2, peforming sample normalisation:
+    x.insert(0, 1)  # Augmentation
     if y == -1:
         x = [i * -1 for i in x]
-        x.insert(0, -1)
-        Norm_Y.append(x)
-    else:
-        x.insert(0, 1)
-        Norm_Y.append(x)
-
-print("Vectors used in Sequential Widrow-Hoff Learning Algorithm:\n {}\n".format(Norm_Y))
+    NORM_X.append(x)
+NORM_X = np.asarray(NORM_X)
 
 # ------------------------------------------------------------------------------------
 # Sequential Widrow-Hoff Learning Algorithm
 # Epoch for-loop: can directly put epoch numbers
-for o in range(int(iterations / len(Norm_Y))): 
+for i in range(iterations):
 
     # This for-loop goes through each sample one-by-one:
-    for i in range(len(Norm_Y)):
+    x, y, margin = list(zip(NORM_X, Y, b))[ i%len(list(zip(NORM_X, Y, b))) ]
 
-        # Value of a to use. If first iteration, then uses parameters given in question:
-        
-        a_prev = a
-        
-        # Which sample to use:
-        y_input = Norm_Y[i]
-        print("Sample used for this iteration is: {}".format(y_input))
+    # Value of a to use. If first iteration, then uses parameters given in question:
+    a_prev = a
 
-        # Equation -> g(x) = a^{t}y
-        ay = np.dot(a, y_input)
-        print("g(x) = {}".format(ay))
+    # Equation -> g(x) = a^{t}y
+    ay = np.dot(a, x)
 
-        # Calculating the values for update:
-        update = np.zeros(len(y_input))
-        for j in range(len(y_input)): 
-            
-            # Applying Update Rule of Sequential Widrow-Hoff Learning Algorithm:
-            update[j] = n * (b[i] - ay) * y_input[j]
+    a = a + (n * (margin - ay) * x)
 
-        # Adding update to a:
-        a = np.add(a_prev, update)
-        print("New Value of a^t is: {}\n".format(a))
+    def converged():
+        return sum((marginsum - np.dot(a, xsum)) * ysum for xsum, ysum, marginsum in zip(NORM_X, Y, b)) < theta
 
-print("Gone through all of the iterations as asked for in question.")
+    if AUTO_CONVERGE and converged():
+        break
+
+print(a)
