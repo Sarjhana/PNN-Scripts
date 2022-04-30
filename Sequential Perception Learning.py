8beed6a7ceeb190d1726ba8bb919b935db159134
jagpdev <78238367+jagpdev@users.noreply.github.com>
2022-04-30 17:08:54 +0100

    Add files via upload

--- Sequential Perception Learning.py
+++ Sequential Perception Learning.py
@@ -2,17 +2,16 @@
 
 import numpy as np
 # ------------------------------------------------------------------------------------
-a = np.array([-1.5, 5.0, -1.0])           # Initial value of a/weights
+a = np.array([1.0, 0.0, 0.0])         # Initial value of a/weights
 n = 1                # Learning Rate
-iterations = 2         # Iterations
-AUTO_CONVERGE = False   # Ignore iterations varible, carry on until convergences
+iterations = 5         # Iterations
 DO_SAMPLE_NORMALISATION = True  # Should this code perform sample normalisation
 
 # Given Dataset:
 
 # Add dataset as given in question.
-X = [[0, 0], [1, 0], [2, 1], [0, 1], [1, 2]]
-Y = np.array([1, 1, 1, -1, -1])
+X = [[0, 2], [1, 2], [2, 1], [-3, 1], [-2, -1], [-3, -2]]
+Y = np.array([1, 1, 1, -1, -1, -1])
 
 # ------------------------------------------------------------------------------------
 # Applying Sample Normalisation:
@@ -28,38 +27,33 @@ NORM_X = np.asarray(NORM_X)
 # ------------------------------------------------------------------------------------
 # Sequential Perceptron Learning Algorithm
 # Epoch for-loop: can directly put epoch numbers
-for o in range(iterations):
+for i in range(iterations):
 
     # This for-loop goes through each sample one-by-one:
-    CONVERGED = True
-    for x, y in zip(NORM_X, Y):
+    x, y = list(zip(NORM_X, Y))[i % len(list(zip(NORM_X, Y)))]
 
-        # Value of a to use. If first iteration, then uses parameters given in question:
-        a_prev = a
+    # Value of a to use. If first iteration, then uses parameters given in question:
+    a_prev = a
 
-        # Equation -> g(x) = a^{t}y
-        ay = np.dot(a, x)
+    # Equation -> g(x) = a^{t}y
+    ay = np.dot(a, x)
 
-        def isMisclassified(ay, y):
-            if DO_SAMPLE_NORMALISATION and ay < 0:
-                return True
-            elif DO_SAMPLE_NORMALISATION:
-                return False
-
-            if ay < 0 and y == 1:
-                return True
-            elif ay > 0 and y == -1:
-                return True
+    def isMisclassified(ay, y):
+        if DO_SAMPLE_NORMALISATION and ay <= 0:
+            return True
+        elif DO_SAMPLE_NORMALISATION:
             return False
 
-        if isMisclassified(ay, y):
-            CONVERGED = False
-            if DO_SAMPLE_NORMALISATION:
-                a = a + (n * x)
-            else:
-                a = a + (n * y * x)
+        if ay <= 0 and y == 1:
+            return True
+        elif ay > 0 and y == -1:
+            return True
+        return False
 
-    if CONVERGED and AUTO_CONVERGE:
-        break
+    if isMisclassified(ay, y):
+        if DO_SAMPLE_NORMALISATION:
+            a = a + (n * x)
+        else:
+            a = a + (n * y * x)
 
 print(a)
