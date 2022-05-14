161e6f7634ed5c971512fc86a912275c86868d81
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2022-05-14 02:00:32 +0100

    Adding graphing

--- Linear Discriminant.py
+++ Linear Discriminant.py
@@ -1,20 +1,31 @@
 # EG Week 2 Q6
 
 import numpy as np 
+from matplotlib import pyplot as plt
 
-a = [-25,6,3]
+a = [1,2]
 
-points = [[1,5],[2,5],[4,1],[5,1]]
-labels = [1,1,2,2]
+points = [[1,2],[2,1],[3,3],[6,5],[7,8]]
+labels = [1,1,1,2,2]
 
 # Augmenting and normalising
 y = [[1] + x if labels[points.index(x)] == 1 else list(np.dot([1] + x, -1)) for x in points]
-y = np.array(y, dtype=np.float64)
+y = np.array(points, dtype=np.float64)
 print(y)
+
+print("a*y: {}".format(np.dot(y, a)))
+
+
+plt.scatter(y[:,0], y[:,1], c=labels)
+plt.grid()
+plt.show()
+
 
 lr = 1
 
 print("Initial a:", a)
+
+
 
 for i in range(5):
 
