348b8dbe363812a1226c7baaf29470cd987fb532
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2022-05-17 00:04:44 +0100

    Adding entropy

--- Decision Trees.py
+++ Decision Trees.py
@@ -36,13 +36,37 @@ class Node():
     def display(self):
         levels_dict = self.dfs(defaultdict(lambda: [], {}), 0)
 
+
         for level in levels_dict:
             print("Level {}".format(level))
             for node in levels_dict[level]:
                 classCount = node[0]
-                p_left = round(classCount[0] / (classCount[0] + classCount[1]), 2)
-                p_right = round(classCount[1] / (classCount[0] + classCount[1]), 2)
-                print(" {}, p_left: {}, p_right: {}, {} |".format(node[0], p_left, p_right, node[1]), end='')
+                p_left = classCount[0] / (classCount[0] + classCount[1])
+                p_right = classCount[1] / (classCount[0] + classCount[1])
+
+                def divWeird(x, y):
+
+                    try:
+                        return x / y
+                    except:
+                        return 0
+
+                def side(l, r):
+                    
+                    a = -divWeird(l , (l + r))
+                    
+                    if a == 0.0:
+                        return 0
+
+                    b = np.log2(-a)
+
+                    return a * b
+
+                entropy = side(p_left, p_right) + side(p_right, p_left)
+
+
+
+                print(" {}, entropy = {}, p_left: {}, p_right: {}, {} |".format(node[0], round(entropy, 2), round(p_left, 2), round(p_right, 2), node[1]), end='')
             print()
 
         print()
@@ -80,49 +104,54 @@ if __name__ == "__main__":
 
 
     # Create a decision tree
+    # If true go right else left
     root = Node(
-        left=Node(
-            left=Node(output=-1),
-            right=Node(output=1),
-            criterion= lambda x: x[1] > 0.5
-            ),
+        left=Node(output=-1),
         right=Node(output=1),
-        criterion=lambda x: x[0] > 0.5
+        criterion=lambda x: x[0] > 144.5
     )
 
-    root2 = Node(
-        left=Node(
-            output=-1
-            ),
-        right=Node(
-            left=Node(output=-1),
-            right=Node(output=1),
-            criterion= lambda x: x[1] > 0.5
-            ),
-        criterion=lambda x: x[1] > -0.5
-    )
+    # root2 = Node(
+    #     left=Node(
+    #         output=-1
+    #         ),
+    #     right=Node(
+    #         left=Node(output=-1),
+    #         right=Node(output=1),
+    #         criterion= lambda x: x[1] > 0.5
+    #         ),
+    #     criterion=lambda x: x[1] > -0.5
+    # )
 
 
     # Enter data from the question
-    X = [[1,0], [-1,0], [0,1], [0,-1]]
-    y = [1, 1, -1, -1]
+    X = [[168, 132, 59],
+    [206, 5, 62],
+    [138, 123, 267],
+    [88, 12, 102],
+    [1, 114, 29],
+    [154, 42, 101],
+    [46, 146, 167],
+    [52, 208, 134],
+    [40, 22, 250]]
+    y = [1,1,-1,-1,1,1,1,1,-1]
 
     zipped = list(zip(X,y))
 
     for x in zipped:
         root.search(x)
-        root2.search(x)
+        # root2.search(x)
 
     root.display()
-    root2.display()
+    # root2.display()
 
     # Search with a new sample
     sample = [[0,0]]
     zipped = list(zip(sample, [None]*len(sample)))
     
-    for x in zipped:
-        root.search(x)
-        root2.search(x)
+    # for x in zipped:
+    #     root.search(x)
+    #     root2.search(x)
     
     
 
